33a39c6f86aed8395b6eb4eb8f05ee27713e825f
haanjihello <okayhuh66@gmail.com>
2023-09-20 16:53:37 +0530

    react 1

---
+++ my-app2/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+//using div
+function App() {
+  return (
+    <div>
+      <h1>What sorcery is this?</h1>
+      
+      </div>
+      );
+    }
+    
+  //using just tags <> </>
+  function App() {
+      return (
+        <>
+          <h1>What sorcery is this?</h1>
+
+          
+          
+          </>
+          );
+        }
+
+
+    
+    export default App;
